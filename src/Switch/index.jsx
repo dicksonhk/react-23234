be9c1dbed495b0af1564d4784c9fff39ae318118
Dickson <12670402+dicksonhk@users.noreply.github.com>
2021-04-09 22:36:05 +0800

    initial commit

---
+++ src/Switch/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+export default class Switch extends React.PureComponent {
+    render() {
+        return <div className="comp-switch">To Be Done</div>;
+    }
+}
